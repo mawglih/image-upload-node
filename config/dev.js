a8d7716ef811a9cdd116b859b5df201213711bce
Oleg Markoff <mawglih@gmail.com>
2019-04-11 22:46:00 -0400

    dev

--- config/dev.js
+++ config/dev.js
@@ -3,6 +3,5 @@ module.exports = {
   googleClientSecret: 'uhwsCQxypZVwWbA0e_HuIaYv',
   mongoURI: 'mongodb://oleg:oleg1967@ds017636.mlab.com:17636/blog-app',
   cookieKey: '123123123',
-  accessKeyId: 'AKIA3ZJHZEARTUCGHCU4',
-  secretAccessKey: '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'
+ 
 };
